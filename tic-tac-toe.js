256d711ddab31725e54fb08196849324dba2c250
TJ <tjdrzewiecki@gmail.com>
2022-11-09 19:23:46 -0500

    Upgraded to Typescript

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,12 +1,6 @@
-const player = (team, name) => {
-    return {
-        team,
-        name
-    }
-};
-
+"use strict";
 const gameController = (() => {
-    const board = ["", "", "", "", "", "", "", "", ""]; 
+    const board = ["", "", "", "", "", "", "", "", ""];
     const solutions = [
         [0, 1, 2],
         [3, 4, 5],
@@ -15,163 +9,126 @@ const gameController = (() => {
         [1, 4, 7],
         [2, 5, 8],
         [0, 4, 8],
-        [2, 4, 6]
+        [2, 4, 6],
     ];
-    const newGameButton = document.querySelector('#new-game');
-    const gameboard = document.querySelector('.gameboard');
-    var turnCounter = 0;
-
-    const checkWin = (board, team, name, solutions) => {
-        var index = [];
-        
-        (function getIndex(board, team) {
-            var i = -1;
-            while ((i = board.indexOf(team, i+1)) != -1) {
-                index.push(i);
+    const newGameButton = document.querySelector("#new-game");
+    const gameboard = document.querySelector("#gameboard");
+    let turnCounter = 0;
+    const checkWin = (player) => {
+        let teamIndex = [];
+        (function getIndex() {
+            let i = -1;
+            while ((i = board.indexOf(player.team, i + 1)) != -1) {
+                teamIndex.push(i);
             }
-            return index;
-        })(board, team);
-
-        (function win(index, solutions, name, board) {
+            return teamIndex;
+        })();
+        (function win() {
             for (let i = 0; i < solutions.length; i++) {
                 for (let j = 0; j < solutions[i].length; j++) {
-                    if (index.includes(solutions[i][j]) == true) {
-                        if (index.includes(solutions[i][j + 1]) == true) {
-                            if (index.includes(solutions[i][ j + 2]) == true) {
-                                alert(name + ' Wins!');
+                    if (teamIndex.includes(solutions[i][j]) == true) {
+                        if (teamIndex.includes(solutions[i][j + 1]) == true) {
+                            if (teamIndex.includes(solutions[i][j + 2]) == true) {
+                                alert(`Team ${player.team}, ${player.name} Wins!`);
                                 reset();
-                                index = [];
-                                gameboard.style.display = 'none';
-                                newGameButton.style.display = 'flex';
                             }
                         }
                     }
                 }
             }
             if (board.includes("") == false) {
-                alert('Tie Game!');
+                alert("Tie Game!");
                 reset();
-                index = [];
-                gameboard.style.display = 'none';
-                newGameButton.style.display = 'flex';
+                teamIndex = [];
             }
-        })(index, solutions, name, board);
+        })();
     };
-
     const createBoard = (playerOne, playerTwo) => {
         for (let i = 0; i < 9; i++) {
-            const div = document.createElement('div');
-            div.classList = ('box');
-            div.id = (i);
-            div.style.display = 'flex';
-            div.style.justifyContent = 'center';
-            div.style.alignItems = 'center';
-            div.style.border = 'ridge white';
-            div.style.font = 'bold 2em fantasy';
-            div.style.color = 'white';
-            div.addEventListener('click', function(){
-                if (div.textContent == '') {
+            const div = document.createElement("div");
+            div.classList.add("box");
+            div.addEventListener("click", function () {
+                if (div.textContent == "") {
                     if (turnCounter % 2 == 0) {
                         div.textContent = playerOne.team;
-                        div.style.color = 'rgba(171, 205, 239, 1)';
-                        div.style.border = '10px groove rgba(171, 205, 239, 1)';
+                        div.style.color = "rgba(171, 205, 239, 1)";
+                        div.style.border = "10px groove rgba(171, 205, 239, 1)";
                         turnCounter++;
-                        changeBoard(div.id, playerOne.team);
-                        checkWin(board, playerOne.team, playerOne.name, solutions);
+                        board[i] = playerOne.team;
+                        checkWin(playerOne);
                     }
                     else {
                         div.textContent = playerTwo.team;
-                        div.style.color = 'rgba(190, 144, 212, 1)';
-                        div.style.border = '10px groove rgba(190, 144, 212, 1)';
+                        div.style.color = "rgba(190, 144, 212, 1)";
+                        div.style.border = "10px groove rgba(190, 144, 212, 1)";
                         turnCounter++;
-                        changeBoard(div.id, playerTwo.team);
-                        checkWin(board, playerTwo.team, playerTwo.name, solutions);
+                        board[i] = playerTwo.team;
+                        checkWin(playerTwo);
                     }
                 }
             });
             gameboard.appendChild(div);
         }
     };
-
-    const reset = (index) => {
+    const reset = () => {
         turnCounter = 0;
-        board.forEach((x, i) => {
+        board.forEach((node, i) => {
             board[i] = "";
         });
         while (gameboard.firstElementChild) {
             gameboard.removeChild(gameboard.firstElementChild);
         }
+        gameboard.style.display = "none";
+        newGameButton.style.display = "flex";
     };
-
-    const changeBoard = (box, team) => {
-        board[box] = team;
-    };
-
     return {
-        board,
-        solutions,
         createBoard,
-        changeBoard,
-        reset,
-    }
+    };
 })();
-
-const displayController = ((gameController) => {
-    const newGameButton = document.querySelector('#new-game');
-    const teamForm = document.querySelector('#team-form');
-    const nameForm = document.querySelector('#name-form');
-    const teamSubmit = document.querySelector('#team-submit');
-    const nameSubmit = document.querySelector('#name-submit');
-    const gameboard = document.querySelector('.gameboard');
-    var name;
-    var team;
-    var players = 0;
-    var playerOne;
-    var playerTwo;
-   
-    newGameButton.addEventListener('click', function(){
-        name;
-        team;
-        players = 0;
-        playerOne;
-        playerTwo;
-        newGameButton.style.display = 'none';
-        teamForm.style.display = 'flex';
+// create game
+const gameCreation = ((gameController) => {
+    const newGameButton = document.querySelector("#new-game");
+    const teamForm = document.querySelector("#team-form");
+    const teamX = document.querySelector("#team-x");
+    const teamSubmit = document.querySelector("#team-submit");
+    const nameForm = document.querySelector("#name-form");
+    const name = document.querySelector("#name-text");
+    const nameSubmit = document.querySelector("#name-submit");
+    const gameboard = document.querySelector("#gameboard");
+    const player = (team, name) => {
+        return {
+            team,
+            name,
+        };
+    };
+    let players = 0;
+    const playerOne = player("", "");
+    const playerTwo = player("", "");
+    newGameButton.addEventListener("click", function () {
+        newGameButton.style.display = "none";
+        teamForm.style.display = "flex";
     });
-
-    teamSubmit.addEventListener('click', function(){
-        teamForm.style.display = 'none';
-        nameForm.style.display = 'flex';
+    teamSubmit.addEventListener("click", function () {
+        teamForm.style.display = "none";
+        nameForm.style.display = "flex";
         if (players == 0) {
-            if (document.querySelector('#team-x').checked) {
-                team = 'X';
-            }
-            else {
-                team = 'O';
-            }
+            teamX.checked ? (playerOne.team = "X") : (playerOne.team = "O");
         }
         else {
-            if (playerOne.team == 'X') {
-                team = 'O';
-            }
-            else {
-                team = 'X';
-            }
+            playerOne.team == "X" ? (playerTwo.team = "O") : (playerTwo.team = "X");
         }
     });
-     
-    nameSubmit.addEventListener('click', function(){
-        nameForm.style.display = 'none';
-        name = document.querySelector('#name-text').value
+    nameSubmit.addEventListener("click", function () {
+        nameForm.style.display = "none";
         if (players == 0) {
-            playerOne = player(team, name);
+            playerOne.name = name.value;
             players++;
-            teamForm.style.display = 'flex';
+            teamForm.style.display = "flex";
         }
         else {
-            playerTwo = player(team, name);
+            playerTwo.name = name.value;
             gameController.createBoard(playerOne, playerTwo);
-            gameboard.style.display = 'grid';
+            gameboard.style.display = "grid";
         }
     });
 })(gameController);
---
+++ tic-tac-toe.ts
@@ -0,0 +1,156 @@
+type Player = {
+  team: string;
+  name: string;
+};
+
+const gameController = (() => {
+  const board = ["", "", "", "", "", "", "", "", ""];
+  const solutions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  const newGameButton = document.querySelector(
+    "#new-game"
+  ) as HTMLButtonElement;
+  const gameboard = document.querySelector("#gameboard") as HTMLDivElement;
+  let turnCounter = 0;
+
+  const checkWin = (player: Player) => {
+    let teamIndex = [];
+
+    (function getIndex() {
+      let i = -1;
+      while ((i = board.indexOf(player.team, i + 1)) != -1) {
+        teamIndex.push(i);
+      }
+      return teamIndex;
+    })();
+
+    (function win() {
+      for (let i = 0; i < solutions.length; i++) {
+        for (let j = 0; j < solutions[i].length; j++) {
+          if (teamIndex.includes(solutions[i][j]) == true) {
+            if (teamIndex.includes(solutions[i][j + 1]) == true) {
+              if (teamIndex.includes(solutions[i][j + 2]) == true) {
+                alert(`Team ${player.team}, ${player.name} Wins!`);
+                reset();
+              }
+            }
+          }
+        }
+      }
+      if (board.includes("") == false) {
+        alert("Tie Game!");
+        reset();
+        teamIndex = [];
+      }
+    })();
+  };
+
+  const createBoard = (playerOne: Player, playerTwo: Player) => {
+    for (let i = 0; i < 9; i++) {
+      const div = document.createElement("div");
+      div.classList.add("box");
+      div.addEventListener("click", function () {
+        if (div.textContent == "") {
+          if (turnCounter % 2 == 0) {
+            div.textContent = playerOne.team;
+            div.style.color = "rgba(171, 205, 239, 1)";
+            div.style.border = "10px groove rgba(171, 205, 239, 1)";
+            turnCounter++;
+            board[i] = playerOne.team;
+            checkWin(playerOne);
+          } else {
+            div.textContent = playerTwo.team;
+            div.style.color = "rgba(190, 144, 212, 1)";
+            div.style.border = "10px groove rgba(190, 144, 212, 1)";
+            turnCounter++;
+            board[i] = playerTwo.team;
+            checkWin(playerTwo);
+          }
+        }
+      });
+      gameboard.appendChild(div);
+    }
+  };
+
+  const reset = () => {
+    turnCounter = 0;
+    board.forEach((node, i) => {
+      board[i] = "";
+    });
+    while (gameboard.firstElementChild) {
+      gameboard.removeChild(gameboard.firstElementChild);
+    }
+    gameboard.style.display = "none";
+    newGameButton.style.display = "flex";
+  };
+
+  return {
+    createBoard,
+  };
+})();
+
+// create game
+
+const gameCreation = ((gameController) => {
+  const newGameButton = document.querySelector(
+    "#new-game"
+  ) as HTMLButtonElement;
+  const teamForm = document.querySelector("#team-form") as HTMLFormElement;
+  const teamX = document.querySelector("#team-x") as HTMLInputElement;
+  const teamSubmit = document.querySelector(
+    "#team-submit"
+  ) as HTMLButtonElement;
+  const nameForm = document.querySelector("#name-form") as HTMLFormElement;
+  const name = document.querySelector("#name-text") as HTMLInputElement;
+  const nameSubmit = document.querySelector(
+    "#name-submit"
+  ) as HTMLButtonElement;
+  const gameboard = document.querySelector("#gameboard") as HTMLDivElement;
+
+  const player = (team: string, name: string) => {
+    return {
+      team,
+      name,
+    };
+  };
+
+  let players = 0;
+  const playerOne = player("", "");
+  const playerTwo = player("", "");
+
+  newGameButton.addEventListener("click", function () {
+    newGameButton.style.display = "none";
+    teamForm.style.display = "flex";
+  });
+
+  teamSubmit.addEventListener("click", function () {
+    teamForm.style.display = "none";
+    nameForm.style.display = "flex";
+    if (players == 0) {
+      teamX.checked ? (playerOne.team = "X") : (playerOne.team = "O");
+    } else {
+      playerOne.team == "X" ? (playerTwo.team = "O") : (playerTwo.team = "X");
+    }
+  });
+
+  nameSubmit.addEventListener("click", function () {
+    nameForm.style.display = "none";
+    if (players == 0) {
+      playerOne.name = name.value;
+      players++;
+      teamForm.style.display = "flex";
+    } else {
+      playerTwo.name = name.value;
+      gameController.createBoard(playerOne, playerTwo);
+      gameboard.style.display = "grid";
+    }
+  });
+})(gameController);
